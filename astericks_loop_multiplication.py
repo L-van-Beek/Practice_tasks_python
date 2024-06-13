953d30a9c2f739e0e6c14114e193bb2691df829b
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-06-13 12:45:41 +0200

    Create astericks_loop_multiplication.py

---
+++ astericks_loop_multiplication.py
@@ -0,0 +1,14 @@
+# Create a program that uses a for loop and range() function to display the following pattern in the terminal using a bunch of asterisks *:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# ... and so on
+
+# It should look like this but with 25 rows total.
+
+# Solution: 
+for astericks in range(1, 25):
+  print(astericks * "*")
