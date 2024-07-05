1be6e94652cc100a6a9fef633bac269e9908d908
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-05 11:22:34 +0200

    Create average.py

---
+++ Functions/average.py
@@ -0,0 +1,17 @@
+# Finding an average can be a useful way to understand the underlying trends of some data.
+# Write an average() function that takes in two parameters:
+
+# - num1
+# - num2
+# It should calculate and return the average of the two numbers.
+
+# The average of num1 and num2 is: (num1+num2)/2
+
+# Code: 
+
+def average(num1, num2):
+  av = (num1 + num2)/2
+  return av
+
+average(3, 5)
+
